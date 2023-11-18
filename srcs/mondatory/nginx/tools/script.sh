02355d6c3d8ce1034207483cc9602129d5d40401
Reda Doukali farji <rdoukali@3-G-7.42heilbronn.de>
2023-11-19 00:51:31 +0100

    rdoukali

--- srcs/mondatory/nginx/tools/script.sh
+++ srcs/mondatory/nginx/tools/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=MO/L=KH/O=1337/OU=student/CN=rdoukali.42.de"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=MO/L=KH/O=1337/OU=student/CN=rdoukali.42heilbronn.de"
 
 {
     echo "server {"
