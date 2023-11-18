3da71cc9e6c557ced1fe76906fb118f4327c63f1
Reda Doukali farji <rdoukali@3-G-7.42heilbronn.de>
2023-11-18 23:18:03 +0100

    rdoukali

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -16,11 +16,11 @@ wp user create ${WP_USR} ${WP_EMAIL} --role=author --user_pass=${WP_PWD} --allow
 # sed -i -r "s/debian/$db_name/1; s/user/$db_user/1; s/pwd/$db_pwd/1" wp-config.php
 # wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --prompt=db
 # wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-wp theme install astra --activate --allow-root
-wp plugin install redis-cache --activate --allow-root
-wp plugin update --all --allow-root
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-mkdir -p /run/php
-wp redis enable --allow-root
+# wp theme install astra --activate --allow-root
+# wp plugin install redis-cache --activate --allow-root
+# wp plugin update --all --allow-root
+# sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+# mkdir -p /run/php
+# wp redis enable --allow-root
 
 /usr/sbin/php-fpm7.4 -F
